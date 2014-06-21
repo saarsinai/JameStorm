6ae1a09225680118a8d7eebb036df3eb034a5f52
Kobi <Mizrahi>
2014-06-21 18:02:20 +0300

    התרעות עכשיו בעיצוב שלנו :)

--- Outer/src/com/JameStorm/outer/TurnLocationOnDialog.java
+++ Outer/src/com/JameStorm/outer/TurnLocationOnDialog.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class TurnLocationOnDialog extends DialogFragment 
 {
@@ -13,9 +14,13 @@ public class TurnLocationOnDialog extends DialogFragment
 	{
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.turn_location_dialog_view, null));
+        /*
         builder.setMessage(R.string.turn_location_message)
-        		.setTitle(R.string.turn_location_title)
-        		.setPositiveButton(R.string.turn_location_on_button, new DialogInterface.OnClickListener() {
+        		.setTitle(R.string.turn_location_title)*/
+        		builder.setPositiveButton(R.string.turn_location_on_button, new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
                 	   startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                    }
